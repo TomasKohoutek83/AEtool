b993a9ae14180137295927d06493daff08e3dfd1
Tomas Kohoutek <Tomas.kohoutek83@gmail.com>
2022-06-13 22:21:13 +0200

    ulozeni

--- sortace.py
+++ sortace.py
@@ -5,7 +5,7 @@ df = pd.read_excel('Duben.xlsx')
 #print(df.columns)
 #print(df['Unnamed: 1'])
 
-print(df[['Order','Operation short text']])
+print(df[['Order','Operation short text','PersNo','PersName','ConfAct']])
 
 
 
